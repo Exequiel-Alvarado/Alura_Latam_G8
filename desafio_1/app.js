e02e9415a02c357047eaad0f57c3156ad1b7998e
Exequiel <alvarado.exequiel@gmail.com>
2025-01-19 19:28:02 -0300

    8/10
    8.Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.

--- desafio_1/app.js
+++ desafio_1/app.js
@@ -1,8 +1,10 @@
 alert('¡Bienvenida y bienvenido a nuestro sitio web!');
-let nombre = "Lua";
+let nombre_1 = "Lua";
 let edad = 25;
 let numeroDeVentas = 50;
 let saldoDisponible = 1000;
 alert('¡Error! Completa todos los campos');
 let mensajeDeError = "¡Error! Completa todos los campos";
 alert(mensajeDeError);
+let nombre = prompt("Cual es tu nombre") ;
+console.log(nombre);
