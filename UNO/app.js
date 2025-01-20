813b9f3d462c3a85ef39d3c8e3ce0838e6507348
Exequiel <alvarado.exequiel@gmail.com>
2025-01-20 02:11:23 -0300

    add bucle

--- UNO/app.js
+++ UNO/app.js
@@ -1,16 +1,25 @@
-let numeroSecreto = 3;
-let numeroUsuario = prompt("Me indicas un numero  entre 1 y 10 por favor");
+let numeroSecreto =5;
+let numeroUsuario =0;
+while (numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero  entre 1 y 10 por favor");
 
-//alert('Hola Mundo');
+    //alert('Hola Mundo');
 
-console.log(numeroUsuario);
+    console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto ){
-    alert (`acertaste el numero de usuario es : ${numeroUsuario}`);
-} else{
-    alert('lo siento, no acertaste')
-};
-
+    if (numeroUsuario == numeroSecreto ){
+        alert (`acertaste el numero de usuario es : ${numeroUsuario}`);
+       
+    } else{
+        if(numeroUsuario > numeroSecreto){
+            alert('el numero secreto es menor');
+        }else{
+            alert('el numero secreto es mayor');
+        }
+        //alert('lo siento, no acertaste')
+    }
+   
+}
 
 
 
