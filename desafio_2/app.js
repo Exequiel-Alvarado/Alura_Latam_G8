6bd8e6b46b00968412858bb1eb3c122291d45566
Exequiel <alvarado.exequiel@gmail.com>
2025-01-19 23:59:57 -0300

    desafio_2 2/5
    2.Verifica si un número ingresado por el usuario es positivo o negativo.
    Muestra una alerta informativa.

--- desafio_2/app.js
+++ desafio_2/app.js
@@ -3,7 +3,7 @@
 Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". 
 De lo contrario, muestra "¡Buena semana!".
 ---------------------------------------------
-*/
+
 
 let semana = prompt("qué día de la semana es");
 console.log(semana);
@@ -13,3 +13,19 @@ if("sabado" == semana |"domingo" == semana ){
 }else{
     alert('Buena semana!');
 }
+*/
+
+/*
+2.Verifica si un número ingresado por el usuario es positivo o negativo. 
+Muestra una alerta informativa.
+------------------------------------------------
+
+*/
+
+let numero = prompt('Ingrese un numero');
+
+if(numero>=0){
+    alert('Tu numero es positivo siendo :' +' ' + numero);
+}else{
+    alert('Tu numero es negativo siendo :' + ' '+ numero)
+};
