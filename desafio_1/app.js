aa18cf2297f8caddfb7f6592441aeefd10595997
Exequiel <alvarado.exequiel@gmail.com>
2025-01-19 22:24:10 -0300

    9/10
    9.Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.

--- desafio_1/app.js
+++ desafio_1/app.js
@@ -1,10 +1,12 @@
 alert('¡Bienvenida y bienvenido a nuestro sitio web!');
 let nombre_1 = "Lua";
-let edad = 25;
+let edad_1 = 25;
 let numeroDeVentas = 50;
 let saldoDisponible = 1000;
-alert('¡Error! Completa todos los campos');
+//alert('¡Error! Completa todos los campos');
 let mensajeDeError = "¡Error! Completa todos los campos";
 alert(mensajeDeError);
 let nombre = prompt("Cual es tu nombre") ;
 console.log(nombre);
+let edad = prompt('ingresa tu edad ');
+console.log(edad);
