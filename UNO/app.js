b4ae35f1b434b109ef2ec800984b0d4b99f522f2
Exequiel <alvarado.exequiel@gmail.com>
2025-01-19 22:37:59 -0300

    continuamos con UNO
    juego de acertar

--- UNO/app.js
+++ UNO/app.js
@@ -1,12 +1,14 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor");
+let numeroUsuario = prompt("Me indicas un numero  entre 1 y 10 por favor");
 
 //alert('Hola Mundo');
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto ){
-    alert ('asertaste el numero')
+    alert ('acertaste el numero')
+} else{
+    alert('lo siento, no acertaste')
 }
 
 
