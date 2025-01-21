d7d4076ba7612eebe85a697d5b61fa28d57d363f
Exequiel <alvarado.exequiel@gmail.com>
2025-01-21 00:36:16 -0300

    update

--- desafio_1/app.js
+++ desafio_1/app.js
@@ -1,16 +1,28 @@
+//1.Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!". OK
 alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+//2.Declara una variable llamada nombre y asígnale el valor "Lua".
 let nombre_1 = "Lua";
+//3.Crea una variable llamada edady asígnale el valor 25.
 let edad_1 = 25;
+//4.Establece una variable numeroDeVentas y asígnale el valor 50.
 let numeroDeVentas = 50;
+//5.Establece una variable saldoDisponible y asígnale el valor 1000.
 let saldoDisponible = 1000;
-//alert('¡Error! Completa todos los campos');
+//6.Muestra una alerta con el texto "¡Error! Completa todos los campos".
+alert('¡Error! Completa todos los campos');
+//7.Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos".Ahora muestra una alerta con el valor de la variable mensajeDeError .
 let mensajeDeError = "¡Error! Completa todos los campos";
 alert(mensajeDeError);
+//8.Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+
 let nombre = prompt("Cual es tu nombre") ;
 console.log(nombre);
+//9.Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
 let edad = prompt('ingresa tu edad ');
 console.log(edad);
-
+//10.Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje
+//"¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, 
+//las soluciones a las actividades estarán disponibles en la sección
 if (edad=18){
     alert('¡Puedes obtener tu licencia de conducir!');
 }
