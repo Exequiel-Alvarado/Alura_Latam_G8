7429bf043321c023be210f5bf5322f85697f645f
Exequiel <alvarado.exequiel@gmail.com>
2025-02-03 10:16:20 -0300

    avances de ejercicio 3

---
+++ desafio_3/app.js
@@ -0,0 +1,34 @@
+/* 1.Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'.
+Muestra cada número.
+
+
+let contador = 1;
+
+while (contador<10){
+    console.log('contado' + contador);
+    alert('este es el numero '+ contador +'');
+    contador = contador +1;
+}
+
+------------------------------------------------------------------------------------
+*/
+
+
+/*2.Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'.
+Muestra cada número.
+
+
+let contador = 10;
+
+while (contador>0){
+    console.log('contado' + contador);
+    alert('este es el numero '+ contador +'');
+    contador = contador -1;
+}
+
+----------------------------------------------------------------------------------
+*/
+
+
+
+
