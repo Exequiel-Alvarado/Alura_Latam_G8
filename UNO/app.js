0ecaf861ff4ef9bbbe90f06de238f75ef5470ce3
Exequiel <alvarado.exequiel@gmail.com>
2025-02-04 17:10:14 -0300

    up en codigo

--- UNO/app.js
+++ UNO/app.js
@@ -1,6 +1,7 @@
 
 //variables
-let numeroSecreto =Math.floor(Math.random()*10)+1;
+let numeroMaximoPosible = Math.floor(Math.random()*100+1);
+let numeroSecreto =Math.floor(Math.random()*numeroMaximoPosible)+1;
 let numeroUsuario =0;
 let intentos=1;
 //let palabraVeces = 'vez';
@@ -9,7 +10,7 @@ let maximoIntentos =3;
 console.log(numeroSecreto);
 
 while (numeroUsuario != numeroSecreto){
-    numeroUsuario = parseInt(prompt("Me indicas un numero  entre 1 y 10 por favor"));
+    numeroUsuario = parseInt(prompt(`Me indicas un numero  entre 1 y ${numeroMaximoPosible}  por favor`));
 
     //alert('Hola Mundo');
 
