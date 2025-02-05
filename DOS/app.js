63e018d30a8c02ed63e497549549b74182b334e6
Exequiel <alvarado.exequiel@gmail.com>
2025-02-05 10:40:29 -0300

    se agrega return

--- DOS/app.js
+++ DOS/app.js
@@ -1,15 +1,25 @@
+let numeroSecreto = generalNumeroSecreto();
+console.log (numeroSecreto);
+
 
 
 function asignarTextoElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 
 
 };
 
 function intentoDeUsuario(){
     alert('click desde el boton');
+    return;
 }
+
+function generalNumeroSecreto(){
+   return  Math.floor(Math.random()*10)+1;
+   
+};
 
 asignarTextoElemento('h1','juego del numero secreto');
 asignarTextoElemento('p','indicar un numero del 1 al 100');
