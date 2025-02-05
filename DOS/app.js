691c50f61f9ff2f3782043c1f0bc25d87914e187
Exequiel <alvarado.exequiel@gmail.com>
2025-02-05 09:50:12 -0300

    se incorpora document.queryselector

---
+++ DOS/app.js
@@ -0,0 +1,7 @@
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
+
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = ' seleciona un numero de 1 al 10';
