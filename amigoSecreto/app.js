ececf4cbc18715abfd1ed03b01b26cdeac16108b
Exequiel <alvarado.exequiel@gmail.com>
2025-02-10 09:38:20 -0300

    actualizar lista amigo secreto

--- amigoSecreto/app.js
+++ amigoSecreto/app.js
@@ -1,9 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Variable array para almacenar los nombres del amigo secreto
 let guardarNombre = [];
 
-
+//funcion para agregar amigos secreto a la lista
 function agregarAmigo(){
     
-    console.log(guardarNombre)
-    return;
+    let ingresarAmigo =document.getElementById('amigo');
+    let amigoSecreto= ingresarAmigo.value.trim();
+ 
+    //verificar campo vacios
+    if (amigoSecreto === ''){
+        alert('por favor, ingresa un nombre valido.');
+        return;
+    }
+
+  
+
+
 }
+
