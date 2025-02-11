635653753898fdda605e2f289c855b19aa89c3a6
Exequiel <alvarado.exequiel@gmail.com>
2025-02-11 09:03:23 -0300

    actualizar lista de amigos

--- amigoSecreto/app.js
+++ amigoSecreto/app.js
@@ -25,12 +25,25 @@ function agregarAmigo (){
 
     //actualizar la lista en pantalla
     actualizarLista ();
-    
+
 }
 
 //funcion para actualixar la listas de amigos
-
+function actualizarLista(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    //limpiar campo
+    listaAmigos.innerHTML=""; //borar contenido previo dentro del contenedor
+    // recorrer el array con un ciclo for
+    for (let i=0 ; i<amigos.length;i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+    
+}
 
 
 
 //funcion para sweleccionar a un amigo
+
+funtion 
